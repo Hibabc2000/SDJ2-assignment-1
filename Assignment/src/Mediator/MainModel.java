ad3cded5d920a414cb4dd103f07b127964247665
GitGudNubHub <58514021+GitGudNubHub@users.noreply.github.com>
2020-03-02 15:41:55 +0100

    MainMod interface,mainmondclass

--- Assignment/src/Mediator/MainModel.java
+++ Assignment/src/Mediator/MainModel.java
@@ -1,5 +1,10 @@
 package Mediator;
 
+import java.beans.PropertyChangeListener;
+
 public interface MainModel
-{
+{void addListener(PropertyChangeListener listener);
+void removeListener(PropertyChangeListener listener);
+void addPropertyChangeListener(String eventName, PropertyChangeListener listener);
+void removePropertyChangeListener(String eventName, PropertyChangeListener listener);
 }
--- Assignment/src/Mediator/MainModelImplementation.java
+++ Assignment/src/Mediator/MainModelImplementation.java
@@ -1,5 +1,9 @@
 package Mediator;
 
+
+import java.beans.PropertyChangeSupport;
+
 public class MainModelImplementation
 {
+  private PropertyChangeSupport support ;
 }
